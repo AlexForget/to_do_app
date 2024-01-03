b3a79a25d5da6fee54eb39f245d7c36286f57026
AlexForget <alexandreqc26@gmail.com>
2024-01-03 09:06:45 -0500

    Add an animation to hide fab when scrolling down

--- lib/src/features/notes/presentation/screen/home_page.dart
+++ lib/src/features/notes/presentation/screen/home_page.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import 'package:to_do_app/src/features/notes/bloc/note_list_bloc.dart';
 import 'package:to_do_app/src/features/notes/presentation/screen/build_note_tile.dart';
 import 'package:to_do_app/src/features/notes/presentation/widget/alert_dialog_add_note.dart';
+import 'package:to_do_app/src/helpers/app_sizes.dart';
 import 'package:to_do_app/src/localisation/string_hardcoded.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,6 +18,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isFabVisible = true;
   @override
   void initState() {
     super.initState();
@@ -23,23 +28,25 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: FloatingActionButton.extended(
-        foregroundColor: Theme.of(context).colorScheme.surface,
-        backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
-        label: Text(AppLocalizations.of(context)!.add),
-        icon: const Icon(Icons.add),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              TextEditingController descriptionController =
-                  TextEditingController();
-              return AlertDialogAddNote(
-                  descriptionController: descriptionController);
-            },
-          );
-        },
-      ),
+      floatingActionButton: isFabVisible
+          ? FloatingActionButton.extended(
+              foregroundColor: Theme.of(context).colorScheme.surface,
+              backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
+              label: Text(AppLocalizations.of(context)!.add),
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    TextEditingController descriptionController =
+                        TextEditingController();
+                    return AlertDialogAddNote(
+                        descriptionController: descriptionController);
+                  },
+                );
+              },
+            )
+          : null,
       appBar: AppBar(
         centerTitle: true,
         title: Text(
@@ -54,12 +61,22 @@ class _HomePageState extends State<HomePage> {
           if ((state is NoteListUpdated || state is NoteListInitial) &&
               state.notes.isNotEmpty) {
             final notes = state.notes;
-            return ListView.builder(
-              itemCount: notes.length,
-              itemBuilder: (context, index) {
-                final note = notes[index];
-                return BuildNoteTile(context: context, note: note);
+            return NotificationListener<UserScrollNotification>(
+              onNotification: (notification) {
+                if (notification.direction == ScrollDirection.forward) {
+                  if (!isFabVisible) setState(() => isFabVisible = true);
+                } else if (notification.direction == ScrollDirection.reverse) {
+                  if (isFabVisible) setState(() => isFabVisible = false);
+                }
+                return true;
               },
+              child: ListView.builder(
+                itemCount: notes.length,
+                itemBuilder: (context, index) {
+                  final note = notes[index];
+                  return BuildNoteTile(context: context, note: note);
+                },
+              ),
             );
           } else {
             return SizedBox(
