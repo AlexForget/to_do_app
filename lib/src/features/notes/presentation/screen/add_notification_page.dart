b5a59f6075be6e99886977c13bb158a94b1940d7
AlexForget <alexandreqc26@gmail.com>
2024-01-12 08:34:49 -0500

    work on add note page

--- lib/src/features/notes/presentation/screen/add_notification_page.dart
+++ lib/src/features/notes/presentation/screen/add_notification_page.dart
@@ -30,108 +30,143 @@ class _AddNotificationState extends State<AddNotification> {
   ];
   DateTime? notificationTime;
   String formattedDate = '';
+  bool notificationIsSelected = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Ajouter une note'.hardcoded),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        elevation: 4.0,
-        shadowColor: Theme.of(context).colorScheme.onBackground,
-      ),
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            textCapitalization: TextCapitalization.sentences,
-            controller: widget.descriptionController,
-            decoration: InputDecoration(
-                hintText: AppLocalizations.of(context)!.addNewNote),
-            minLines: 1,
-            maxLines: 5,
+      body: Padding(
+        padding: const EdgeInsets.all(Sizes.p24),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surfaceVariant,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(20),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3), // changes position of shadow
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: Sizes.p20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          child: Padding(
+            padding: const EdgeInsets.all(Sizes.p16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                ElevatedButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.background,
-                  ),
-                  onPressed: () async {
-                    setNotification();
-                  },
+                Text(
+                  AppLocalizations.of(context)!.record,
+                  style: TextStyle(
+                      backgroundColor: Theme.of(context).colorScheme.primary),
+                ),
+                TextField(
+                  textCapitalization: TextCapitalization.sentences,
+                  controller: widget.descriptionController,
+                  decoration: InputDecoration(
+                      hintText: AppLocalizations.of(context)!.addNewNote),
+                  minLines: 1,
+                  maxLines: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: Sizes.p20),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: Sizes.p12),
-                        child: Text(
-                          AppLocalizations.of(context)!.addReminder,
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.primary,
-                          ),
+                      ElevatedButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.background,
                         ),
-                      ),
-                      Icon(
-                        Icons.calendar_today,
-                        color: Theme.of(context).colorScheme.primary,
+                        onPressed: () async {
+                          setNotification();
+                        },
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right: Sizes.p12),
+                              child: Text(
+                                AppLocalizations.of(context)!.addReminder,
+                                style: TextStyle(
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
+                              ),
+                            ),
+                            Icon(
+                              Icons.calendar_today,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: Sizes.p16, bottom: Sizes.p16),
+                  child: notificationIsSelected
+                      ? Text(
+                          textAlign: TextAlign.center,
+                          '${AppLocalizations.of(context)!.notificationWillBeSent} $formattedDate')
+                      : null,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: Sizes.p16),
+                  child: DropdownMenu<String>(
+                    label: Text('Kind of notification'.hardcoded),
+                    menuHeight: 200,
+                    enabled: notificationIsSelected,
+                    onSelected: (notificationType) {
+                      print(notificationType);
+                    },
+                    initialSelection: notificationType.first,
+                    dropdownMenuEntries: notificationType
+                        .map<DropdownMenuEntry<String>>((String value) {
+                      return DropdownMenuEntry(value: value, label: value);
+                    }).toList(),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    child: Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(AppLocalizations.of(context)!.cancel),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (widget.descriptionController.text.isNotEmpty) {
+                              final note = NoteModel(
+                                description:
+                                    widget.descriptionController.text.trim(),
+                                completed: false,
+                                notification: notificationTime,
+                              );
+                              context
+                                  .read<NoteListBloc>()
+                                  .add(AddNote(note: note));
+                              Navigator.pop(context);
+                              widget.descriptionController.text = '';
+                            }
+                          },
+                          child: Text(AppLocalizations.of(context)!.record),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: Sizes.p16, bottom: Sizes.p16),
-            child: notificationTime != null
-                ? Text(
-                    textAlign: TextAlign.center,
-                    '${AppLocalizations.of(context)!.notificationWillBeSent} $formattedDate')
-                : null,
-          ),
-          DropdownMenu<String>(
-            initialSelection: notificationType.first,
-            dropdownMenuEntries:
-                notificationType.map<DropdownMenuEntry<String>>((String value) {
-              return DropdownMenuEntry(value: value, label: value);
-            }).toList(),
-          ),
-          Expanded(
-            child: Align(
-              alignment: FractionalOffset.bottomCenter,
-              child: Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(AppLocalizations.of(context)!.cancel),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (widget.descriptionController.text.isNotEmpty) {
-                        final note = NoteModel(
-                          description: widget.descriptionController.text.trim(),
-                          completed: false,
-                          notification: notificationTime,
-                        );
-                        context.read<NoteListBloc>().add(AddNote(note: note));
-                        Navigator.pop(context);
-                        widget.descriptionController.text = '';
-                      }
-                    },
-                    child: Text(AppLocalizations.of(context)!.record),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
@@ -144,6 +179,7 @@ class _AddNotificationState extends State<AddNotification> {
       is24HourMode: true,
     );
     if (notificationTime != null) {
+      notificationIsSelected = true;
       setState(() {
         formattedDate =
             DateFormat.MMMd(deviceLocal).add_Hm().format(notificationTime!);
